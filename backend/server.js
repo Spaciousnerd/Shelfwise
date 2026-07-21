73f24edbfff4b3d4fdb736dbc497fd35ebbdf207
Shivam Yadav <shivamyadav456732@gmail.com>
2026-07-21 20:16:42 +0530

    adding live links

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,12 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 //middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://shelfwise-lib.onrender.com"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(errorHandler);
 
